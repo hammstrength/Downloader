5ab9a79de5c8dc35349c4346bac085ced068ce12
hammstrength <hammstrength@gmail.com>
2025-03-10 01:12:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("7596905535:AAF9tHFxRfihU8Krmz2HV7JJiQpHYDlo-G4")
 APP_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 
